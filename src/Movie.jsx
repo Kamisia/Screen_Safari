368a33e4cab407e86275059050242ab69522b9fa
Kamisia <kamila.samczuk@gmail.com>
2024-02-20 14:47:05 +0100

    deleted:    src/Movie.jsx 	deleted:    src/MovieList.jsx

--- src/Movie.jsx
+++
@@ -1,8 +0,0 @@
-const Movie = ({ posterPath }) => {
-  return (
-    <div className="single-movie">
-      <img src={`https://image.tmdb.org/t/p/w500${posterPath}`} />
-    </div>
-  );
-};
-export default Movie;
