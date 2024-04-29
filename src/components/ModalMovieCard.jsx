25fc43a2afe73ca4a4d0495da23129a724f9bc83
Kamisia <kamila.samczuk@gmail.com>
2024-04-29 11:11:03 +0200

    styling ModalMovieCard.jsx

--- src/components/ModalMovieCard.jsx
+++ src/components/ModalMovieCard.jsx
@@ -6,25 +6,26 @@ const ModalMovieCard = () => {
   const { title, description, posterPath, voteAverage } = modalContent;
   return isOpenModal ? (
     <div className="modal">
-      <div className="modal-content">
-        <button onClick={closeModal}>
-          {" "}
-          <IoMdClose />{" "}
-        </button>
-        <div
-          className="image-container"
-          style={{
-            backgroundImage: posterPath
-              ? `url(https://image.tmdb.org/t/p/w500${posterPath})`
-              : `url(https://cdn.pixabay.com/photo/2018/01/05/00/20/test-image-3061864_1280.png)`,
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-          }}
-        ></div>
+      <div
+        className="modal-content"
+        style={{
+          backgroundImage: posterPath
+            ? `url(https://image.tmdb.org/t/p/w500${posterPath})`
+            : `url(https://cdn.pixabay.com/photo/2018/01/05/00/20/test-image-3061864_1280.png)`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
         <div className="description-content">
+          <div className="btn">
+            <button onClick={closeModal}>
+              {" "}
+              <IoMdClose />{" "}
+            </button>
+          </div>
           <h1>{title}</h1>
-          <h3>{voteAverage} /10</h3>
+          <h3>{voteAverage}/10</h3>
           <p>{description} </p>
         </div>
       </div>
